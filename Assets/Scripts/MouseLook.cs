39ff96dc43a6a4a1b63fd230c731e236e6f3dad7
Peter155 <pgw25@drexel.edu>
2022-05-09 17:59:26 -0400

    Simple player movement and Camera movement
    Player can sprint for set amount of time and stamina recovers slowly. Player camera follows player mouse.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public bool inverted;
+
+    public Transform playerBody;
+
+    float xRotaion = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    //Camera follows the mouse, mouse is locked to center of screen when running and can't look all the way up and around
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        if (inverted)
+        {
+            xRotaion += mouseY;
+        } else
+        {
+            xRotaion -= mouseY;
+        }
+        
+        xRotaion = Mathf.Clamp(xRotaion, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotaion, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float sprintSpeed = 20f;
+    public float playerStamina = 2f;
+
+    public bool sprinting = false;
+
+    private float stamMax;
+
+    private void Start()
+    {
+        stamMax = playerStamina;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //Sprints when player hits shift unless no stamina, ends sprint when shift lifted
+        if (Input.GetKeyDown(KeyCode.LeftShift) && playerStamina > 0)
+        {
+            sprinting = true;
+        } else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            sprinting = false;
+        }
+
+        //Sends the stamina to negative if player runs until stamina runs out
+        if (playerStamina <= 0 && sprinting)
+        {
+            sprinting = false;
+            playerStamina = -4f;
+        }
+
+        //Moves faster when sprinting but consumes stamina, when not moves slower and restores stamina slower to a cap
+        if (sprinting)
+        {
+            controller.Move(move * sprintSpeed * Time.deltaTime);
+            playerStamina -= 1 * Time.deltaTime;
+        } else
+        {
+            controller.Move(move * speed * Time.deltaTime);
+            playerStamina += 0.5f * Time.deltaTime;
+            playerStamina = Mathf.Clamp(playerStamina, -4f, stamMax);
+        }
+        
+    }
+}
