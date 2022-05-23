acfef6550b8dbf545b5d919d10b32f9f74b9ee35
Ricky Chen <rc989@drexel.edu>
2022-05-23 08:37:13 -0400

    Added the enemy AI (not complete) and edited the main level to include the enemy

--- Assets/Scripts/ExitSpawner.cs
+++ Assets/Scripts/ExitSpawner.cs
@@ -6,16 +6,20 @@ public class ExitSpawner : MonoBehaviour
 {
     // Used to randomly select an exit and placing the enemy at that exit
 
-    public Transform enemy;
+    public GameObject enemy;
 
     //to add more exits, add more shelf prefabs under the exit spawner prefab
     void Start()
     {
         // Gets a random integer to randomly select an exit shelf to be set inactive
         int randomShelf = Random.Range(0, this.transform.childCount);
-        enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
 
-        enemy.position = this.transform.GetChild(randomShelf).transform.position;
+        // Gets the enemy spawn location and sets it as the enemy's position as well as setting its mesh renderer active
+        Vector3 enemySpawnLocation = this.transform.GetChild(randomShelf).transform.position;
+        enemy.transform.position = new Vector3(enemySpawnLocation.x, 1, enemySpawnLocation.z);
+        enemy.GetComponent<MeshRenderer>().enabled = true;
+
         this.transform.GetChild(randomShelf).gameObject.SetActive(false);
     }
 }
